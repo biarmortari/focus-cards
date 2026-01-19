ba3e996885089883d85bab879f9f901215ae316f
biarmortari <beatrizrmortari@gmail.com>
2026-01-19 12:26:54 -0300

    feat(main): provide flashcards context in main to enable shared state access

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+import "./index.css";
+
+import App from "./App";
+
+import { FlashcardsProvider } from "../src/contexts/FlashcardsContext";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <FlashcardsProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FlashcardsProvider>
+  </StrictMode>,
+);
