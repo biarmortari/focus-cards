bcd0ac7a6a9d5044cecfbd58052d03ea85ea6269
biarmortari <beatrizrmortari@gmail.com>
2026-01-21 15:52:56 -0300

    feat(AllCards): add form to create new flashcards

--- src/components/FlashcardForm/FlashcardForm.jsx
+++ src/components/FlashcardForm/FlashcardForm.jsx
@@ -1,4 +1,40 @@
+import { useState, useContext } from "react";
+import { FlashcardsContext } from "../../contexts/FlashcardsContext";
+
 export function FlashcardForm() {
+  const [data, setData] = useState({
+    question: "",
+    answer: "",
+    category: "",
+  });
+
+  const { addFlashcard } = useContext(FlashcardsContext);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!data.question || !data.answer || !data.category) {
+      return;
+    }
+
+    addFlashcard(data);
+
+    setData({
+      question: "",
+      answer: "",
+      category: "",
+    });
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
   return (
     <form className="flashcard_form" onSubmit={handleSubmit}>
       <fieldset className="form__fieldset">
@@ -25,6 +61,19 @@ export function FlashcardForm() {
           value={data.answer}
           onChange={handleChange}
           className="form__input"
+        />
+        <span className="form__input-error"></span>
+
+        <p>Category</p>
+        <input
+          id="category"
+          name="category"
+          type="text"
+          placeholder="e.g., Geography"
+          value={data.category}
+          onChange={handleChange}
+          className="form__input"
+          required
         />
         <span className="form__input-error"></span>
 
