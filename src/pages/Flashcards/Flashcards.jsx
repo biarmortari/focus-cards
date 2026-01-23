52f9d82f7ce783c364d287387f0fca36e8b9fa1a
biarmortari <beatrizrmortari@gmail.com>
2026-01-23 19:12:17 -0300

    style(flashcards): add styles to page header

--- src/pages/Flashcards/Flashcards.jsx
+++ src/pages/Flashcards/Flashcards.jsx
@@ -2,21 +2,33 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { AllCards } from "../Flashcards/AllCards/AllCards";
 import { StudyMode } from "../Flashcards/StudyMode/StudyMode";
+import "./Flashcards.css";
 
 export function Flashcards() {
   const [mode, setMode] = useState("all");
 
   return (
-    <>
-      <header>
+    <div className="flashcards__wrapper">
+      <header className="flashcards__header">
         <Link to="/">
-          <button>Voltar para a home</button>
+          <button className="header__button-home">
+            {" "}
+            &#8592; Voltar para a home
+          </button>
         </Link>
-        <div>
-          <button onClick={() => setMode("study")} disabled={mode === "study"}>
+        <div className="header__button-mode">
+          <button
+            onClick={() => setMode("study")}
+            disabled={mode === "study"}
+            className={`header__button ${mode === "study" ? "active" : ""}`}
+          >
             Study Mode
           </button>
-          <button onClick={() => setMode("all")} disabled={mode === "all"}>
+          <button
+            onClick={() => setMode("all")}
+            disabled={mode === "all"}
+            className={`header__button ${mode === "all" ? "active" : ""}`}
+          >
             All Cards
           </button>
         </div>
@@ -25,6 +37,6 @@ export function Flashcards() {
         {mode === "all" && <AllCards />}
         {mode === "study" && <StudyMode />}
       </main>
-    </>
+    </div>
   );
 }
