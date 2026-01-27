a400ae38147c38b53b28f312c3b27446c0408570
biarmortari <beatrizrmortari@gmail.com>
2026-01-27 12:02:39 -0300

    feat(progressbar): create and style progress bar component

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,1 +1,18 @@
-export function ProgressBar() {}
+import "./ProgressBar.css";
+
+export function ProgressBar({ value }) {
+  const max = 5;
+  const percentage = Math.min((value / max) * 100, 100);
+
+  return (
+    <div className="progress">
+      <div className="progress__bar">
+        <div className="progress__fill" style={{ width: `${percentage}%` }} />
+      </div>
+
+      <span className="progress__text">
+        {value} / {max}
+      </span>
+    </div>
+  );
+}
