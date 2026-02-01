32ee86bf22a01b67fb74eccf1401b9b1ae8fe79d
biarmortari <beatrizrmortari@gmail.com>
2026-01-31 23:20:31 -0300

    feat: create PomodoroControls component

---
+++ src/components/PomodoroWidget/PomodoroControls/PomodoroControls.jsx
@@ -0,0 +1,17 @@
+import { usePomodoro } from "../../contexts/PomodoroContext";
+
+export function PomodoroControls() {
+  const { isRunning, start, pause, reset } = usePomodoro();
+
+  return (
+    <div className="pomodoro__controls">
+      {!isRunning ? (
+        <button onClick={start}>Start</button>
+      ) : (
+        <button onClick={pause}>Pause</button>
+      )}
+
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
